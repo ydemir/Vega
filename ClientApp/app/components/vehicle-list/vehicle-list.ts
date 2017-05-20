b0da06fdb71da56efde795624b3a6ac60ffccf05
Yusuf Demir <yusuf@bilgiedin.net>
2017-05-20 15:04:15 +0300

    Section 7, Lecture 100 Client tarafındaki filter işlemi tamamlandı.

--- ClientApp/app/components/vehicle-list/vehicle-list.ts
+++ ClientApp/app/components/vehicle-list/vehicle-list.ts
@@ -1,4 +1,4 @@
-import { Vehicle } from './../../models/vehicle';
+import { Vehicle, KeyValuePair } from './../../models/vehicle';
 import { VehicleService } from './../../services/vehicle.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,11 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehicleListComponent implements OnInit {
     vehicles: Vehicle[];
+    allVehicles: Vehicle[];
+    makes: KeyValuePair[];
+    filter: any = {};
 
     constructor(private vehicleService: VehicleService) { }
 
     ngOnInit() {
+        this.vehicleService.getMakes()
+            .subscribe(makes => this.makes = makes);
         this.vehicleService.getVehicles()
-            .subscribe(vehicles => this.vehicles = vehicles);
+            .subscribe(vehicles => this.vehicles=this.  allVehicles = vehicles);
+    }
+
+    onFilterChange() {
+        var vehicles = this.allVehicles;
+
+        if (this.filter.makeId) {
+            vehicles = vehicles.filter(v => v.make.id == this.filter.makeId);
+        }
+
+        if (this.filter.modelId) {
+            vehicles = vehicles.filter(v => v.model.id == this.filter.modelId);
+        }
+        this.vehicles = vehicles;
+    }
+    resetFilter() {
+        this.filter = {};
+        this.onFilterChange();
     }
 }
