c496d155353bf48e1386db38778fff4c647fda02
Yusuf Demir <yusuf@bilgiedin.net>
2017-05-20 16:05:29 +0300

    Section 7, Lecture 102 tamamlandı

--- Core/Models/Filter.cs
+++ Core/Models/Filter.cs
@@ -8,6 +8,7 @@ namespace Vega.Core.Models
     public class Filter
     {
         public int? MakeId { get; set; }
+        public int? ModelId { get; set; }
 
     }
 }
--- Persistance/VehicleRepository.cs
+++ Persistance/VehicleRepository.cs
@@ -56,6 +56,10 @@ namespace Vega.Persistance
             {
                 query = query.Where(v => v.Model.MakeId == filter.MakeId.Value);
             }
+            if (filter.ModelId.HasValue)
+            {
+                query = query.Where(v => v.ModelId == filter.ModelId.Value);
+            }
             return await query.ToListAsync();
           
         }
