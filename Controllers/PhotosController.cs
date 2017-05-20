433894e488a3aa817cbe69407b379af4b999de27
Yusuf Demir <yusuf@bilgiedin.net>
2017-05-21 00:58:10 +0300

    Section 8, Lecture 120 build api tamamlandı

---
+++ Controllers/PhotosController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Vega.Controllers.Resources;
+using Vega.Core;
+using Vega.Core.Models;
+
+namespace Vega.Controllers
+{
+    [Route("/api/vehicles/{vehicleId/photos")]
+
+    public class PhotosController : Controller
+    {
+        private readonly IHostingEnvironment host;
+        private readonly IVehicleRepository repository;
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+        public PhotosController(IHostingEnvironment host, IVehicleRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            this.host = host;
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Upload(int vehicleId, IFormFile file)
+        {
+            var vehicle = await repository.GetVehicle(vehicleId, includeRelated: false);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            var uploadFolderPath = Path.Combine(host.WebRootPath, "uploads");
+            if (!Directory.Exists(uploadFolderPath))
+            {
+                Directory.CreateDirectory(uploadFolderPath);
+            }
+
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.Name);
+            var filePath = Path.Combine(uploadFolderPath, fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            var photo = new Photo
+            {
+                FileName = fileName
+            };
+            vehicle.Photos.Add(photo);
+            await unitOfWork.CompleteAsync();
+            return Ok(mapper.Map<Photo, PhotoResource>(photo));
+        }
+    }
+}
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -14,6 +14,7 @@ namespace Vega.Mapping
         public MappingProfile()
         {
             //Domain to API Resource
+            CreateMap<Photo, PhotoResource>();
             CreateMap(typeof(QueryResult<>), typeof(QueryResultResource<>));
             CreateMap<Make, MakeResource>();
             CreateMap<Make, KeyValuePairResource>();
